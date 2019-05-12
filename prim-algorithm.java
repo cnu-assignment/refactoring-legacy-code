6ff189da6679730f98632e52daf9dfbaa1771393
Yongjae Yi <dididy@knou.ac.kr>
2019-05-12 19:59:29 +0900

    Add: 3 more refactoring attribute

--- prim-algorithm.java
+++ prim-algorithm.java
@@ -22,6 +22,7 @@ public class Graph {
 
     public Graph(String args) { //그래프 생성자 메소드
         try {
+/**  Rename method  **/
             edge e = new edge();
             BufferedReader reader = new BufferedReader(new FileReader(args));
             String line = reader.readLine(); //파일로부터 한 줄 읽어 String으로 변환
@@ -38,7 +39,7 @@ public class Graph {
 
             line = reader.readLine(); //한 줄 읽으면 다음 줄로 커서가 넘어가므로 line에 또 파일의 한 줄을 읽어 String으로 변환한다
 
-
+/**  Introduce Null Object  **/
             while(line!=null) { //line이 null이 아니라면
                 String listSplit[]=line.split(" "); //문자열을 구분자로 분해해 배열에 저장
                 e.v = Integer.parseInt(listSplit[0]);
@@ -73,6 +74,7 @@ public class Graph {
 
 
         for(int i=0; i<size; i++) {
+/**  Extract method  **/
             near[i]=-1; //near은 초기에 -1값으로 초기화
         }
 
@@ -101,8 +103,10 @@ public class Graph {
                     for(int j=0; j<size; j++) {
                         if(include[j]) {
                             for(int k=0;k<size;k++) {
+/**  Extract Variable  **/
                                 if(adjacency[j][k]!=Integer.MAX_VALUE&&adjacency[j][k]>0&&!include[k]) {
                                     near[k]=j;
+/**  Extract Variable  **/
                                     if(adjacency[j][k]<minWeight) {
                                         minWeight = adjacency[j][k];
                                         minIndex=k;
